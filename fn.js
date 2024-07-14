ecf28c778069f794697533a1adb78e375dc20a80
sono <rhj080471@gmail.com>
2024-07-14 22:43:46 +0900

    jest로 간단한 테스트 코드 작성해보기

---
+++ fn.js
@@ -0,0 +1,22 @@
+// 테스트할 함수 모음
+const fn = {
+  add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({ name, age, gender: undefined }),
+  throwErr: () => {
+    throw new Error("사용자 정의 에러 발생");
+  },
+  callback_returnNameAfter3: (name, callback) => {
+    setTimeout(() => {
+      callback(name);
+    }, 3000);
+  },
+  promise_returnAgeAfter3: (age, callback) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(callback(age));
+      }, 3000);
+    });
+  },
+};
+
+module.exports = fn;
